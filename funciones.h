d09302b6329f76c1b2c7f621de0ae0e2a0b2d28a
JuanGaz2403 <juan.garces.penafiel@udla.edu.ec>
2025-07-06 13:23:27 -0500

    Create funciones.h

---
+++ funciones.h
@@ -0,0 +1,34 @@
+#define MAX_ZONAS 5
+#define MAX_ALERTAS 10
+struct Zona {
+    char nombre[32];
+    float co2, so2, no2, pm25;
+    float temperatura, viento, humedad;
+};
+
+struct Sistema {
+    struct Zona zonas[MAX_ZONAS];
+    int numZonas;
+    char fecha[11]; // YYYY-MM-DD
+};
+
+// Prototipos de funciones principales
+void inicializarSistema(struct Sistema *s);
+int cargarDatosHistoricos(struct Sistema *s, const char *ruta);
+int guardarDatos(struct Sistema *s, const char *ruta);
+void calcularPromedios(struct Sistema *s, float promedios[]);
+void predecirContaminacion(struct Sistema *s, float prediccion[]);
+void emitirAlertas(struct Sistema *s, float prediccion[], char alertas[][64], int *nAlertas);
+void generarRecomendaciones(char alertas[][64], int nAlertas);
+void mostrarTablaZonas(struct Sistema *s);
+void mostrarMenuPrincipal();
+void manejarOpcion(int opcion, struct Sistema *s);
+int leerEntero(const char *prompt, int min, int max, int allowCancel);
+float leerFloat(const char *prompt, float min, float max, int allowCancel);
+int confirmar(const char *mensaje);
+void ayudaMenu(const char *menuNombre);
+void actualizarFechaManualmente(struct Sistema *s);
+// Funciones de ICA y alertas mejoradas
+const char* calcularICA(float pm25);
+void mostrar_alerta(float pm25, const char* zona);
+void registrarPredicciones(struct Sistema *s, float prediccion[]);
